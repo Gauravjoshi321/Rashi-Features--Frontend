feb5331459bca75da77309cbd71a386a14457b25
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-01-03 14:37:12 +0530

    React-router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Route, Routes, useNavigation } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Dashboard from "./Pages/Dashboard";
+import GlobalStyle from "./style/GlobalStyle";
+
 function App() {
+  const Navigate = useNavigation;
 
   return (
     <>
-      App
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            element={
+              <AppLayout />
+            }
+          >
+            <Route index element={<Navigate replace to="dashboard" />} />
+
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
