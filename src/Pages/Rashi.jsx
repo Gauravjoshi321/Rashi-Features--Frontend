1215c58a55494f3938816687964d83971340cfa6
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-01-10 13:26:43 +0530

    correction done

--- src/Pages/Rashi.jsx
+++ src/Pages/Rashi.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import RashiImages from "../features/rashi/RashiImages";
 import RashiKeyData from "../features/rashi/RashiKeyData";
 import RashiHeading from "../features/rashi/RashiHeading";
+import RashiPosNeg from "../features/rashi/RashiPosNeg";
 
 const data = {
   _id: {
@@ -17,20 +18,20 @@ const data = {
     "J"
   ],
   specialQuality: "Resilient Determination",
-  positives: {
-    1: "Capricorn individuals are highly ambitious. They're driven to succeed and often set high goals for themselves.",
-    2: "Capricorn has a practical and realistic approach to life. They're excellent at making plans and organizing tasks efficiently.",
-    3: "Capricorn possesses a strong sense of discipline and responsibility. They're often reliable and can handle tasks that require commitment.",
-    4: "Capricorn is steadfast and determined. They're willing to put in the hard work necessary to achieve their objectives.",
-    5: "Capricorn individuals are dependable and trustworthy. They take their commitments seriously and follow through on their promises."
-  },
-  negatives: {
-    1: "Capricorn individuals can be reserved, making it challenging for them to express their emotions openly. This might sometimes create barriers in relationships.",
-    2: "Their practical nature can sometimes border on pessimism, especially when they focus on potential challenges or obstacles.",
-    3: "Capricorn can be stubborn, especially when they believe their methods or viewpoints are the most effective.",
-    4: "Capricorn's strong work ethic can sometimes lead to overworking, neglecting personal time, or finding it challenging to relax.",
-    5: "Capricorn tends to be cautious, often preferring to stay in their comfort zone rather than taking big risks."
-  },
+  positives: [
+    "Capricorn individuals are highly ambitious. They're driven to succeed and often set high goals for themselves.",
+    "Capricorn has a practical and realistic approach to life. They're excellent at making plans and organizing tasks efficiently.",
+    "Capricorn possesses a strong sense of discipline and responsibility. They're often reliable and can handle tasks that require commitment.",
+    "Capricorn is steadfast and determined. They're willing to put in the hard work necessary to achieve their objectives.",
+    "Capricorn individuals are dependable and trustworthy. They take their commitments seriously and follow through on their promises."
+  ],
+  negatives: [
+    "Capricorn individuals can be reserved, making it challenging for them to express their emotions openly. This might sometimes create barriers in relationships.",
+    "Their practical nature can sometimes border on pessimism, especially when they focus on potential challenges or obstacles.",
+    "Capricorn can be stubborn, especially when they believe their methods or viewpoints are the most effective.",
+    "Capricorn's strong work ethic can sometimes lead to overworking, neglecting personal time, or finding it challenging to relax.",
+    "Capricorn tends to be cautious, often preferring to stay in their comfort zone rather than taking big risks."
+  ],
   startDate: "2001-12-21T18:30:00.000Z",
   endDate: "2001-01-18T18:30:00.000Z",
   description: "Capricorn is the tenth sign of the zodiac, symbolized by the Goat with a fish tail. People born between December 22 and January 19 fall under this sign. Capricorn is an Earth sign, and it is ruled by Saturn, the planet of discipline, responsibility, and structure.",
@@ -56,6 +57,7 @@ function Rashi() {
       <RashiImages images={data.image} />
       <RashiKeyData data={data} />
 
+      <RashiPosNeg data={data} />
     </StyledRashi >
   )
 }
--- src/features/rashi/RashiPosNeg.jsx
+++ src/features/rashi/RashiPosNeg.jsx
@@ -1,8 +1,47 @@
-function RashiPosNeg() {
-  return (
-    <div>
+import styled from "styled-components";
 
-    </div>
+const StyledRashiPosNeg = styled.div`
+  grid-row: 3 / -1;
+  grid-column: 1 / -1;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`
+
+const Positives = styled.div`
+
+`
+
+const Negatives = styled.div`
+  
+`
+
+const Heading = styled.h3`
+  font-size: 1.8rem;
+  font-weight: 600;
+  color: var(--color-grey-600);
+
+`
+
+const PositiveData = styled.ul`
+  
+`
+
+function RashiPosNeg({ data }) {
+  return (
+    <StyledRashiPosNeg>
+      <Positives>
+        <Heading>Positives</Heading>
+        <PositiveData>
+
+        </PositiveData>
+      </Positives>
+
+      <Negatives>
+        <Heading>Negatives</Heading>
+      </Negatives>
+    </StyledRashiPosNeg>
   )
 }
 
