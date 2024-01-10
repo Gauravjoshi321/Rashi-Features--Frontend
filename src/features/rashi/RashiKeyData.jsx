37b2da93094d1eb4eb5b27c0ad58894ec9057f20
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-01-10 11:08:41 +0530

    RashiKeyData

---
+++ src/features/rashi/RashiKeyData.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components"
+import { endDateCalculator, startDateCalculator } from "../../utils/dateCalculators"
+
+const StyledKeyData = styled.div`
+  grid-row: 2 / 3;
+  padding: 2rem 2rem;
+  height: 12rem;
+  font-weight: 500;
+  letter-spacing: 0.8px;
+  color: var(--color-grey-500);
+
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  gap: 2.2rem;
+  align-items: center;
+`
+
+const KeyDataNameTime = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`
+
+
+const TimeCalc = styled.ul`
+  display: flex;
+  gap: 1.6rem;
+`
+
+const Description = styled.p`
+  font-size: 1.3rem;
+  line-height: 1.9;
+  padding: 2rem;
+  color: var(--color-grey-400);
+  background-color: #e3dfee;
+  border-radius: 0.8rem;
+  `
+
+function RashiKeyData({ data }) {
+  return (
+    <StyledKeyData>
+      <KeyDataNameTime>
+        <p>Name Letters: {data.nameLetter.join(", ")}</p>
+
+        <TimeCalc>
+          <li>From: {startDateCalculator(data)}</li>
+          <li>To: {endDateCalculator(data)}</li>
+        </TimeCalc>
+      </KeyDataNameTime>
+
+      <Description>{data.description}</Description>
+    </StyledKeyData>
+  )
+}
+
+export default RashiKeyData;
